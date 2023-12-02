e8083c6427036dae1b454d027bd3c6164a70faf0
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2023-12-02 16:07:14 +0100

    Improved solutions

--- 2023/AdventOfCode2023/2.swift
+++ 2023/AdventOfCode2023/2.swift
@@ -44,6 +44,26 @@ func sumOfGameIds() -> Int {
     return sumOfGameIds
 }
 
+func sumOfGameIdsImproved() -> Int {
+    let colorLimits: [String: Int] = ["red": 12, "green": 13, "blue": 14]
+    return games.split(separator: "\n").compactMap { line -> Int? in
+        let parts = line.split(separator: ": ")
+        let rounds = parts[1].split(separator: "; ")
+        for round in rounds {
+            let cubes = round.split(separator: ", ")
+            for cube in cubes {
+                let valueAndColor = cube.split(separator: " ")
+                if let value = Int(valueAndColor[0]),
+                    let limit = colorLimits[String(valueAndColor[1])],
+                    value > limit {
+                    return nil
+                }
+            }
+        }
+        return Int(parts[0].split(separator: " ")[1])
+    }.reduce(0, +)
+}
+
 func sumOfPowerOfGames() -> Int {
     var sumOfGamePowers = 0
     let lines = games.split(separator: "\n")
@@ -80,5 +100,24 @@ func sumOfPowerOfGames() -> Int {
     }
     
     return sumOfGamePowers
+}
+
+func sumOfPowerOfGamesImproved() -> Int {
+    return games.split(separator: "\n").map { line -> Int in
+        let game = line.split(separator: ": ")
+        let rounds = game[1].split(separator: "; ")
+        var highestValues: [String: Int] = ["red": 0, "green": 0, "blue": 0]
+        for round in rounds {
+            round.split(separator: ", ").forEach { cube in
+                let valueAndColor = cube.split(separator: " ")
+                let color = String(valueAndColor[1])
+                if let value = Int(valueAndColor[0]),
+                    value > highestValues[color, default: 0] {
+                    highestValues[color] = value
+                }
+            }
+        }
+        return highestValues.values.reduce(1, *)
+    }.reduce(0, +)
 }
 
--- 2023/AdventOfCode2023/main.swift
+++ 2023/AdventOfCode2023/main.swift
@@ -1,4 +1,4 @@
 import Foundation
 
-print(sumOfPowerOfGames())
+print(sumOfPowerOfGamesImproved())
 
