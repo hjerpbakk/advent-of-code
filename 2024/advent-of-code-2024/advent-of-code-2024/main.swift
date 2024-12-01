787587768dc5d3a4093151e1ba43b77b78ffe9ac
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2024-12-01 12:25:31 +0100

    2024.1

---
+++ 2024/advent-of-code-2024/advent-of-code-2024/main.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func measureExecutionTime<T>(for function: () -> T, label: String) -> T {
+    let start = Date()
+    let result = function()
+    let end = Date()
+    
+    let timeInterval = end.timeIntervalSince(start)
+    let milliseconds = timeInterval * 1000
+    let formattedTime = String(format: "%.3f", milliseconds)
+    
+    print(result)
+    print("\(label) finished in \(formattedTime) ms")
+    print()
+    
+    return result
+}
+
+let result1 = measureExecutionTime(for: day1_part1, label: "Part 1")
+let result2 = measureExecutionTime(for: day1_part2, label: "Part 2")
